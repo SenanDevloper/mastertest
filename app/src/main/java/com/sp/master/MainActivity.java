71e3e4b1320b0077b37c349e6fb6ba9ccd740761
mohammedsenanmasoud <fastecit2020>
2023-04-12 23:50:54 +0300

    frial bransh version

--- app/src/main/java/com/sp/master/MainActivity.java
+++ app/src/main/java/com/sp/master/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this master version   1.0.0
-        //this frial version   1.0.0
+        //this senan version   1.0.0
     }
 }
