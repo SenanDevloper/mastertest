62f65e292fc64fdf5a386a43d0df95719d19be5e
mohammedsenanmasoud <fastecit2020>
2023-04-12 23:11:26 +0300

    master version

--- app/src/main/java/com/sp/master/MainActivity.java
+++ app/src/main/java/com/sp/master/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this master version
+        //this master version   1.0.0
     }
 }
